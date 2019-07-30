d22ff8b1c820913b6145104d11b9edae182ba1d3
Volodymyr Kryvytskyy <volodymyrkryvytskyy@Volodymyrs-MBP.mate.academy>
2019-07-30 18:25:54 +0300

    Updates

---
+++ precache-manifest.b1268baa13c5ae4edabe2fb6fa14460f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2599fefd11931a1b397fefff4f48d7",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "e4d88e9374f77cfb75c9",
+    "url": "/react_redux-list-of-todos/static/css/main.5534d4f3.chunk.css"
+  },
+  {
+    "revision": "868c5c1c2ab16b9df5f1",
+    "url": "/react_redux-list-of-todos/static/js/2.a3be3563.chunk.js"
+  },
+  {
+    "revision": "e4d88e9374f77cfb75c9",
+    "url": "/react_redux-list-of-todos/static/js/main.0a93bf15.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
